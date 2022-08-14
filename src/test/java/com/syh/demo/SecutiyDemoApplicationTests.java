e4faff0550a60db52144779f6929311a9756a13e
shiyinhong <shiyingongsama@foxmail.com>
2022-08-14 16:32:42 +0800

    first commit

---
+++ src/test/java/com/syh/demo/SecutiyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syh.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecutiyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
