386dd4815a84b6317f020d01f244bd20fc386d18
shiyinhong <shiyingongsama@foxmail.com>
2022-08-16 17:26:37 +0800

    update

---
+++ userdetails/src/main/java/com/syh/demo/config/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package com.syh.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //TODO
+        return null;
+    }
+
+}
