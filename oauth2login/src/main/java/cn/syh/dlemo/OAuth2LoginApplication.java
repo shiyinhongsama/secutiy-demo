0b426d4edb7eba73b69339b44bd02bf1eaed51eb
石印鸿 <shiyh@yunphant.com>
2022-08-25 18:04:46 +0800

    update

---
+++ oauth2login/src/main/java/cn/syh/dlemo/OAuth2LoginApplication.java
@@ -0,0 +1,18 @@
+package cn.syh.dlemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shiyinhong
+ * @ClassName OAuth2LoginApplication
+ * @description: OAuth2登录
+ * @datetime 2022年 08月 25日 15:36
+ * @company Hangzhou Yunphant internet technology co.ltd
+ */
+@SpringBootApplication
+public class OAuth2LoginApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2LoginApplication.class, args);
+    }
+}
